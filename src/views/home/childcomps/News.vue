27caf6b1c8180b24f2b09c97ab78c18491f29ea3
黄子维 <9678142+yeeee8@user.noreply.gitee.com>
2022-05-06 15:35:45 +0800

    change css

--- src/views/home/childcomps/News.vue
+++ src/views/home/childcomps/News.vue
@@ -2,26 +2,30 @@
   <div id="news" class="w">
     <h2>新闻动态</h2>
     <div class="new">
-      <div class="n-item">
-      <img src="~assets/img/news/pic1.jpg" alt="">
-      <div class="jiaodian">焦点新闻</div>
-      <h3>国际奥委会主席巴赫：中国为全球冰雪运动开启新时代</h3>
-      <el-button type="danger"><a href="https://www.beijing2022.cn/wog.htm?cmsid=20220206002394">阅读更多</a></el-button>
-    </div>
+      <a class="n-item" href="https://www.beijing2022.cn/wog.htm?cmsid=20220206002394" target="_blank">
+        <img src="~assets/img/news/pic1.jpg" alt="">
+        <div class="intro">
+          <div class="jiaodian">焦点新闻</div>
+          <h3>国际奥委会主席巴赫：中国为全球冰雪运动开启新时代</h3>   
+        </div>    
+      </a>
 
-    <div class="n-item">
-      <img src="~assets/img/news/pic2.jpg" alt="">
-      <div class="jiaodian">焦点新闻</div>
-      <h3>国际残奥委会主席帕森斯：北京冬残奥会把办赛标准提高到了新水平</h3>
-      <el-button type="danger"><a href="https://www.beijing2022.cn/paralympics/wog.htm?cmsid=20220312050909">阅读更多</a></el-button>
-    </div>
+      <a class="n-item" href="https://www.beijing2022.cn/paralympics/wog.htm?cmsid=20220312050909" target="_blank">
+        <img src="~assets/img/news/pic2.jpg" alt="">
+        <div class="intro">
+          <div class="jiaodian">焦点新闻</div>
+          <h3>国际残奥委会主席帕森斯：北京冬残奥会把办赛标准提高到了新水平</h3> 
+        </div>    
+      </a>
 
-    <div class="n-item">
-      <img src="~assets/img/news/pic3.jpg" alt="">
-      <div class="jiaodian">焦点新闻</div>
-      <h3>弘扬北京冬奥精神 续写“双奥之城”荣耀 在新起点上更加奋发有为推动新时代首都发展</h3>
-      <el-button type="danger"><a href="https://www.beijing2022.cn/wog.htm?cmsid=20220419007506">阅读更多</a></el-button>
-    </div>
+      <a class="n-item" href="https://www.beijing2022.cn/wog.htm?cmsid=20220419007506" target="_blank">
+        <img src="~assets/img/news/pic3.jpg" alt="">
+        <div class="intro">
+          <div class="jiaodian">焦点新闻</div>
+          <h3>弘扬北京冬奥精神 续写“双奥之城”荣耀 在新起点上更加奋发有为推动新时代首都发展</h3>
+        </div>     
+      </a>
+
     </div>
   </div>
 </template>
@@ -59,14 +63,21 @@ export default {
 }
 
 .n-item {
+  display: block;
   width: 420px;
-  height: 500px;
+  height: 400px;
   text-align: center;
+  transition:  0.4s;
 }
 
 .n-item img {
   width: 100%;
   object-fit: cover;
+}
+
+.n-item:hover {
+  transform: translateY(-20px) scale(1.05);
+  box-shadow: 0px 0px 36px rgba(0,0,0,0.1);
 }
 
 .jiaodian {
@@ -80,12 +91,11 @@ export default {
   color: #fff;
 }
 
-.el-button {
-  margin-top: 22px;
+h3 {
+  text-align: left;
 }
 
-.el-button a {
-  font-size: 16px;
-  color: #fff;
+.intro {
+  padding: 0 30px;
 }
 </style>
