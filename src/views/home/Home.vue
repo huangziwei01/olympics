87be580079458b94ed775699f169db10cffca28e
黄子维 <9678142+yeeee8@user.noreply.gitee.com>
2022-05-06 15:16:53 +0800

    delete backTop

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -7,7 +7,7 @@
     <History></History>
     <Spirit></Spirit>
     <News></News>
-    <back-top>回到顶部</back-top>
+    <!-- <back-top>回到顶部</back-top> -->
   </div>
 </template>
 
@@ -20,7 +20,7 @@ import History from './childcomps/History'
 import Spirit from './childcomps/Spirit'
 import News from './childcomps/News'
 
-import BackTop from 'components/common/BackTop'
+// import BackTop from 'components/common/BackTop'
 
 export default {
   name: '',
@@ -36,7 +36,7 @@ export default {
     HomeHeader,
     History,
     Spirit,
-    BackTop,
+    // BackTop,
     News
   }
 }
